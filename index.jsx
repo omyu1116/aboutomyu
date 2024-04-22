94cc687471cd91ff9a33dab6d6ed16d380880baa
omyu <165384786+omyu1116@users.noreply.github.com>
2024-04-22 10:50:29 +0900

    jsx

---
+++ index.jsx
@@ -0,0 +1,28 @@
+<>
+  <meta charSet="UTF-8" />
+  <link rel="stylesheet" href="style.css" />
+  <title>omyu's profile</title>
+  <link
+    rel='”icon"'
+    href="https://raw.githubusercontent.com/omyu1116/aboutomyu/main/favicon.ico"
+  />
+  <header>
+    <h1 className="headline">
+      <a href="index.html">omyu's profile!</a>
+    </h1>
+  </header>
+  <h2 className="intro">
+    <a href="introduce.html">自己紹介</a>
+  </h2>
+  <h2 className="intro">
+    <a href="bloglist.html">ブログ</a>
+  </h2>
+  <p>ただの暇人であるおみゅが勉強しながら一から作ってるサイト</p>
+  <p>
+    <img
+      src="https://github.com/omyu1116/aboutomyu/blob/main/canvas.png?raw=true"
+      alt="omyuAA"
+    />
+  </p>
+  <button onclick="switchTheme()" />
+</>
